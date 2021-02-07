6502c9ee9d49244eb311c6b2ac8663812d95fbf7
Ricardo D. Cuero <47835668+merolemay@users.noreply.github.com>
2021-02-07 02:45:43 -0500

    Interface and basic Excel Load code

---
+++ Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+using System.Data;
+using System.Data.OleDb;
+
+namespace DataBaseImportador
+{
+    class Importar
+    {
+        OleDbConnection conn;
+        OleDbDataAdapter MyDataAdapter;
+        DataTable dt;
+
+        public void importarExcel(DataGridView dgv, String nombreHoja)
+        {
+            String ruta = "";
+            try
+            {
+                OpenFileDialog openfile1 = new OpenFileDialog();
+                openfile1.Filter = "Excel Files |*.xlsx";
+                openfile1.Title = "Seleccione el archivo de Excel";
+                if (openfile1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    if (openfile1.FileName.Equals("") == false)
+                    {
+                        ruta = openfile1.FileName;
+                    }
+                }
+
+                conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;data source=" + ruta + ";Extended Properties='Excel 12.0 Xml;HDR=Yes'");
+                MyDataAdapter = new OleDbDataAdapter("Select * from [" + nombreHoja + "$]", conn);
+                dt = new DataTable();
+                MyDataAdapter.Fill(dt);
+                dgv.DataSource = dt;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using DataBaseImportador;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataBase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            new Importar().importarExcel(dataGridView1, "DIVIPOLA-_C_digos_municipios");
+        }
+
+    }
+}
